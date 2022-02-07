33bcd994962e6f190206a0e7a7ccde5f35e9ff6a
Ben Carpenter <carpenter.ben@icloud.com>
2022-02-07 12:41:24 -0600

    improve user interface

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::io::{Write};
 use std::fs;
 use std::process;
 
@@ -8,22 +9,23 @@ fn main() {
 	let mut current_guess = ['S', 'L', 'I', 'C', 'E'];
 	let mut dict_vec:Vec<[char; 5]> = read_dict();
 
+	print_starting_status();
 	loop {
-		print_status(correct_chars, &known_chars);
-		println!("Next guess: {}", String::from_iter(current_guess));
-		println!("            01234");
-		println!("Solved? y/n");
+		print_status(correct_chars, &known_chars, current_guess);
+		print!("Solved? (y/n) > ");
+		let _ = io::stdout().flush();
 		let mut answer = String::new();
-
+		
 		io::stdin()
-			.read_line(&mut answer)
-			.unwrap();
-
+		.read_line(&mut answer)
+		.unwrap();
+		
 		match answer.as_str().trim() {
 			"y" => break,
 			"n" => evaluate_guess(&mut correct_chars, &mut known_chars, &mut current_guess),
-			  _ => println!("Please respond with y/n."),
+			_ => println!("Please respond with y/n."),
 		}
+		std::process::Command::new("clear").status().unwrap();
 		current_guess = update_guess(&mut dict_vec, &mut known_chars, correct_chars);
 	}
 	println!("Nice!");
@@ -51,26 +53,29 @@ fn read_dict() -> Vec<[char; 5]> {
 }
 
 // Queries the user for the outcome of the guess.
+// TODO: Sanitize input
 fn evaluate_guess(correct_chars: &mut [char], known_chars: &mut String, current_guess: &mut [char]) {
 	let mut correct_chars_input = String::new();
 	let mut known_chars_input = String::new();
 
-	println!("Are any new letters in the right position (0..4)?");
+	print!("Are any new letters in the right position (1..5)? > ");
+	let _ = io::stdout().flush();
 	io::stdin()
 		.read_line(&mut correct_chars_input)
 		.unwrap();
 
-	println!("Are any new letters in the wrong position (0..4)?");
+	print!("Are any new letters in the wrong position (1..5)? > ");
+	let _ = io::stdout().flush();
 	io::stdin()
 		.read_line(&mut known_chars_input)
 		.unwrap();
-	
+
 	for character in correct_chars_input.chars() {
 		if character < '0' || character > '9' {
 			continue;
 		}
 		let integer = character.to_digit(10).unwrap() as usize;
-		correct_chars[integer] = current_guess[integer];
+		correct_chars[integer - 1] = current_guess[integer - 1];
 	}
 
 	for character in known_chars_input.chars() {
@@ -80,7 +85,7 @@ fn evaluate_guess(correct_chars: &mut [char], known_chars: &mut String, current_
 			continue;
 		} else {
 			let integer = character.to_digit(10).unwrap() as usize;
-			known_chars.push(current_guess[integer]);
+			known_chars.push(current_guess[integer - 1]);
 		}
 	}
 }
@@ -99,24 +104,21 @@ fn update_guess(dict_vec: &mut Vec<[char; 5]>, known_chars: &mut String, correct
 		}
 		char_idx += 1;
 	}
-	
-	println!("Pruned {} word(s) with rightly positioned letters.", original_dict_size - dict_vec.len());
 
 	if dict_vec.len() <= 0 {
 		println!("No possibilities remain.");
 		process::exit(0);
 	}
 
-
 	//check contained letters
-	let original_dict_size = dict_vec.len();
 	for character in known_chars.chars() {
 		if !character.is_ascii_alphabetic() {
 			continue;
 		}
 		dict_vec.retain(|&x| x.contains(&character));
 	}
-	println!("Pruned {} word(s) with wrongly positioned letters.", original_dict_size - dict_vec.len());
+	
+	println!("Pruned {} word(s).", original_dict_size - dict_vec.len());
 	
 	if dict_vec.len() <= 0 {
 		println!("No possibilities remain.");
@@ -127,8 +129,8 @@ fn update_guess(dict_vec: &mut Vec<[char; 5]>, known_chars: &mut String, correct
 }
 
 // Print a status message to the console.
-fn print_status(correct_chars:[char; 5], known_chars: &str) {
-	println!("Status: ");
+fn print_status(correct_chars:[char; 5], known_chars: &str, current_guess:[char; 5]) {
+	print!("    Status: ");
 	for character in correct_chars {
 		if character == '\0' {
 			print!("_");
@@ -144,5 +146,11 @@ fn print_status(correct_chars:[char; 5], known_chars: &str) {
 			print!("{} ", character);
 		}
 	}
-	print!("\n\n");
+	println!("\nNext guess: {}", String::from_iter(current_guess));
+	println!("            12345");
+}
+
+fn print_starting_status() {
+	std::process::Command::new("clear").status().unwrap();
+	println!("Pruned -- word(s).");
 }
